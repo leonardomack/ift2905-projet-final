00667eb0d55a67cf02757dd8a3e46786efc90647
Vincent Dupuis <vincentdupuis263@hotmail.com>
2014-04-02 12:23:32 -0400

    Correction du bug qui faisait que la progressBar s'affichait à l'ouverture de l'activité + orgrnisation des import et méthodes non utilisés

--- NobelPrize/src/com/example/nobelprize/LaureateDetailActivity.java
+++ NobelPrize/src/com/example/nobelprize/LaureateDetailActivity.java
@@ -1,17 +1,14 @@
 package com.example.nobelprize;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.text.InputFilter.LengthFilter;
 import android.util.Log;
-import android.widget.*;
 
 public class LaureateDetailActivity extends Activity {
 	private String TAG;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		String TAG = "LaureateDetailActivity";
+		TAG = "LaureateDetailActivity";
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.laureate_detail_layout);
 		//Retrieve Id from intent
